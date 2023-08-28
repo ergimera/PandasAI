9838388233b98c4aafcd277553859a6de52f2a70
Ergi <133374363+ergimera@users.noreply.github.com>
2023-08-28 13:40:56 +0200

    Ok

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import openai
 from pandasai.llm.azure_openai import AzureOpenAI
 import streamlit as st 
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')  # Use a GUI backend
 
 openai.api_key = "87c7609674b14cd3b1518b4d2229ee96"
 openai.api_base = "https://openai-cs-test01.openai.azure.com/"
@@ -13,26 +15,7 @@ openai.api_version = '2023-05-15'
 deployment_name = 'DaVinci03-test'
 
 st.title("Prompt-driven data analysis with PandasAI")
-uploaded_file = st.file_uploader("Upload a CSV file for analysis", type=['csv'])
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.write(df.head(3))
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.write(df.head(3))
-
-    # new code below...
-    prompt = st.text_area("Enter your prompt:")
-
-    # Generate output
-    if st.button("Generate"):
-        if prompt:
-            st.write("Generating response...")
-        else:
-            st.warning("Please enter a prompt.")
-
+uploaded_file = st.file_uploader("Upload a Excel file for analysis", type=['xlsx'])
 
 # Instantiate a LLM
 llm = AzureOpenAI(api_token=openai.api_key, api_base=openai.api_base, api_version=openai.api_version, deployment_name=deployment_name)
@@ -41,7 +24,7 @@ llm = AzureOpenAI(api_token=openai.api_key, api_base=openai.api_base, api_versio
 pandas_ai = PandasAI(llm)
 
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_excel(uploaded_file)
     st.write(df.head(3))
     prompt = st.text_area("Enter your prompt:")
 
@@ -53,3 +36,4 @@ if uploaded_file is not None:
                 st.write(pandas_ai.run(df, prompt))
         else:
             st.warning("Please enter a prompt.")
+
